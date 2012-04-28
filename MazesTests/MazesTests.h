4288504822ebe7133c7c8c8472af4bbef6476125
Andre Muis <Andre@AndreMuis.com>
2012-04-28 16:47:56 -0700

    Initial Commit

---
+++ MazesTests/MazesTests.h
@@ -0,0 +1,13 @@
+//
+//  MazesTests.h
+//  MazesTests
+//
+//  Created by Andre Muis on 4/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MazesTests : SenTestCase
+
+@end
---
+++ MazesTests/MazesTests.m
@@ -0,0 +1,32 @@
+//
+//  MazesTests.m
+//  MazesTests
+//
+//  Created by Andre Muis on 4/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MazesTests.h"
+
+@implementation MazesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MazesTests");
+}
+
+@end
