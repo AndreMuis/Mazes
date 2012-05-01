d7cac081decc495cf68949e8b85c5251f65e5984
Andre Muis <Andre@AndreMuis.com>
2012-04-30 22:47:38 -0700

    added version

---
+++ Mazes/Version.h
@@ -0,0 +1,13 @@
+//
+//  Version.h
+//  Mazes
+//
+//  Created by Andre Muis on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Version : NSObject
+
+@end
---
+++ Mazes/Version.m
@@ -0,0 +1,13 @@
+//
+//  Version.m
+//  Mazes
+//
+//  Created by Andre Muis on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Version.h"
+
+@implementation Version
+
+@end
