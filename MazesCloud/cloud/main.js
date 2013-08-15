4cb1944d7b29a9026f1087b8de6716c3b548debd
Andre Muis <andre@push.am>
2013-08-15 09:52:12 -0700

    removed Pods directory and Parse code

--- MazesCloud/cloud/main.js
+++
@@ -1,5 +0,0 @@
-require('cloud/getHighestRatedMazes.js');
-require('cloud/getNewestMazes.js');
-require('cloud/getCurrentUserMazes.js');
-require('cloud/updateMazeRatings.js');
-require('cloud/utilities.js');
--- Podfile
+++ Podfile
@@ -1,9 +1,5 @@
 platform :ios, '6.0'
 
-pod 'RestKit', :git => 'https://github.com/RestKit/RestKit.git', :branch => 'development'
-
 pod 'FlurrySDK'
 
 pod 'CrittercismSDK'
-
-pod 'Parse'
